b74090642f8802e2c98da5273f67998448a8e0c8
Evan McFarland <62383100+evanmcfarland@users.noreply.github.com>
2020-04-17 09:07:50 -0400

    Add files via upload

---
+++ pay_gap_proj.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[40]:
+
+
+get_ipython().magic('matplotlib inline')
+import pandas as pd
+import matplotlib.pyplot as plt
+
+women_degrees = pd.read_csv('percent-bachelors-degrees-women-usa.csv')
+cb_dark_blue = (0/255,107/255,164/255)
+cb_orange = (255/255, 128/255, 14/255)
+stem_cats = ['Engineering', 'Computer Science', 'Psychology', 'Biology', 'Physical Sciences', 'Math and Statistics']
+
+fig = plt.figure(figsize=(18, 6))
+
+for sp in range(0,6):
+    ax = fig.add_subplot(2,3,sp+1)
+    ax.plot(women_degrees['Year'], women_degrees[stem_cats[sp]], c=cb_dark_blue, label='Women', linewidth=3)
+    ax.plot(women_degrees['Year'], 100-women_degrees[stem_cats[sp]], c=cb_orange, label='Men', linewidth=3)
+    ax.spines["right"].set_visible(False)    
+    ax.spines["left"].set_visible(False)
+    ax.spines["top"].set_visible(False)    
+    ax.spines["bottom"].set_visible(False)
+    ax.tick_params(bottom='off', top='off', left='off', right='off', labelbottom='off')
+    ax.set_yticks([0,100]) 
+    ax.axhline(50, c=(171/255, 171/255, 171/255), alpha=0.3)
+    ax.set_xlim(1968, 2011)
+    ax.set_ylim(0,100)
+    ax.set_title(stem_cats[sp])
+    ax.tick_params(bottom="off", top="off", left="off", right="off")
+    
+    if sp == 0:
+        ax.text(2005, 87, 'Men')
+        ax.text(2002, 8, 'Women')
+    elif sp == 5:
+        ax.text(2005, 62, 'Men')
+        ax.text(2001, 35, 'Women') 
+        
+        
+other_cats = ['Health Professions', 'Public Administration', 'Education', 
+              'Agriculture','Business', 'Architecture']
+
+fig = plt.figure(figsize=(18, 6))
+
+for sp in range(0,6):
+    ax = fig.add_subplot(2,3,sp+1)
+    ax.plot(women_degrees['Year'], women_degrees[other_cats[sp]], c=cb_dark_blue, label='Women', linewidth=3)
+    ax.plot(women_degrees['Year'], 100-women_degrees[other_cats[sp]], c=cb_orange, label='Men', linewidth=3)
+    ax.spines["right"].set_visible(False)    
+    ax.spines["left"].set_visible(False)
+    ax.spines["top"].set_visible(False)    
+    ax.spines["bottom"].set_visible(False)
+    ax.tick_params(bottom='off', top='off', left='off', right='off', labelbottom='off')
+    ax.set_yticks([0,100])
+    ax.axhline(50, c=(171/255, 171/255, 171/255), alpha=0.3)
+    ax.set_xlim(1968, 2011)
+    ax.set_ylim(0,100)
+    ax.set_title(other_cats[sp])
+    ax.tick_params(bottom="off", top="off", left="off", right="off")
+    
+    if sp == 0:
+        ax.text(2005, 87, 'Men')
+        ax.text(2002, 8, 'Women')
+    elif sp == 5:
+        ax.text(2005, 62, 'Men')
+        ax.text(2001, 35, 'Women')
+          
+        
+lib_arts_cats = ['Foreign Languages', 'English', 'Communications and Journalism', 
+                 'Art and Performance', 'Social Sciences and History']
+
+fig = plt.figure(figsize=(18, 6))
+
+for sp in range(0,5):
+    ax = fig.add_subplot(2,3,sp+1)
+    ax.plot(women_degrees['Year'], women_degrees[lib_arts_cats[sp]], c=cb_dark_blue, label='Women', linewidth=3)
+    ax.plot(women_degrees['Year'], 100-women_degrees[lib_arts_cats[sp]], c=cb_orange, label='Men', linewidth=3)
+    ax.spines["right"].set_visible(False)    
+    ax.spines["left"].set_visible(False)
+    ax.spines["top"].set_visible(False)    
+    ax.spines["bottom"].set_visible(False)
+    ax.tick_params(bottom='off', top='off', left='off', right='off', labelbottom='off')
+    ax.set_yticks([0,100])
+    ax.axhline(50, c=(171/255, 171/255, 171/255), alpha=0.3)
+    ax.set_xlim(1968, 2011)
+    ax.set_ylim(0,100)
+    ax.set_title(lib_arts_cats[sp])
+    ax.tick_params(bottom="off", top="off", left="off", right="off")
+    
+    if sp == 2:
+        ax.tick_params(labelbottom='on')
+    if sp == 3:
+        ax.tick_params(labelbottom='on')
+    if sp == 4:
+        ax.tick_params(labelbottom='on')
+    
+    if sp == 0:
+        ax.text(2005, 87, 'Men')
+        ax.text(2002, 8, 'Women')
+    elif sp == 4:
+        ax.text(2005, 62, 'Men')
+        ax.text(2001, 35, 'Women')
+    
+plt.show()
+
+
+# In[43]:
+
+
+plt.savefig('gender_degrees.png')
+plt.get_backend()
+
